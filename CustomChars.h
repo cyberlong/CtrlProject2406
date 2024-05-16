3a693e73dd1bac53a9a3fffc9b98af5f820790d3
cyberlong <53883240+cyberlong@users.noreply.github.com>
2024-05-16 17:30:07 -0500

    Create CustomChars.h

---
+++ CustomChars.h
@@ -0,0 +1,26 @@
+// Display names
+#define OK 1
+#define NO 0
+
+// symbols
+byte todobn[] = { // [✔️] symbol
+  B00001,
+  B00011,
+  B00010,
+  B00010,
+  B00110,
+  B10110,
+  B11100,
+  B01000
+};
+
+byte noks[] = { // [NO] symbol
+  B10010,
+  B11010,
+  B10110,
+  B10010,
+  B00000,
+  B00111,
+  B00101,
+  B00111
+};
